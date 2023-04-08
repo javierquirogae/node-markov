05ce03aa11bcdfe91459a3ea755c0eef75ef9b4f
jav <javier.quiroga.e@gmail.com>
2023-04-08 16:59:00 -0400

    assignment complete

--- makeText.js
+++ makeText.js
@@ -1,2 +1,66 @@
 /** Command-line tool to generate Markov text. */
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process");
+const { MarkovMachine } = require("./markov");
+
+
+// arg is the first command line argument
+const arg = process.argv[2];
+// inputArg is the second command line argument
+const inputArg = process.argv[3];
+
+
+
+// cat webCat are now asynchronous functions that take a callback
+function cat(path, cb) {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        console.error(`Error reading ${path} :\n ${err}`);
+        process.exit(1);
+      }
+      cb(null, data);
+    });
+  }
+ 
+// webCat is now an asynchronous function that takes a callback
+function webCat(url, cb) {
+    axios.get(url)
+      .then(response => cb(null, response.data))
+      .catch(error => cb(error));
+  }
+
+// if arg is 'file', use cat to read inputArg and set output to write to a file
+if (arg === 'file') {
+    cat(inputArg, (err, data) => {
+        if (err) {
+            console.log(err);
+            process.exit(1);
+        }
+        // create new MarkovMachine instance
+        let mm = new MarkovMachine(data);
+        // only write output to the console
+        console.log(mm.makeText());
+    });
+// if arg is 'url', use webCat to read inputArg and set output to write to a file
+} else if (arg === 'url') {
+    webCat(inputArg, (err, data) => {
+        if (err) {  
+            console.log(err);
+            process.exit(1);
+        }
+         // create new MarkovMachine instance
+         let mm = new MarkovMachine(data);
+         // only write output to the console
+         console.log(mm.makeText());
+    });
+// if arg is anything else, use arg as the text and set output to console.log
+} else {
+    output = console.log(`${markov.makeText(arg)} is not a valid argument`);
+}
+
+
+
+
 
--- markov.js
+++ markov.js
@@ -17,13 +17,45 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+      
+      let chains = new Map();
+      // loop through words array
+      for (let i = 0; i < this.words.length; i++) {
+        // set word and nextWord variables
+        let word = this.words[i];
+        let nextWord = this.words[i + 1] || null;
+        // if word is already in chains, push nextWord to its array
+        if (chains.has(word)) chains.get(word).push(nextWord);
+        // else, set word as key and nextWord as value in chains  
+        else chains.set(word, [nextWord]);
+      }
+      // console.log(chains);
+      this.chains = chains;
+
   }
 
 
-  /** return random text from chains */
+  
 
-  makeText(numWords = 100) {
-    // TODO
+  makeText(numWords = 1000) {
+    // get array of keys from chains
+    let keys = Array.from(this.chains.keys());
+    // pick a random key to begin
+    let key = keys[Math.floor(Math.random() * keys.length)];
+    // initialize output array
+    let output = [];
+
+    // produce markov chain until reaching termination word
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      // set key to random value from its array in chains
+      key = this.chains.get(key)[Math.floor(Math.random() * this.chains.get(key).length)];
+    }
+
+    return output.join(" ");
   }
+}
+//export
+module.exports = {
+    MarkovMachine
 }
